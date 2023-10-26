203f291500888c2cdc3fe86c55399587d5e57d5b
Cemothy <118116381+Cemothy@users.noreply.github.com>
2023-10-26 16:15:18 +0200

    Added Prints for debugging

--- SuperMarket/Customer.py
+++ SuperMarket/Customer.py
@@ -28,19 +28,54 @@ class Customer:
     def arrive_station(self, t):
         self.actual_task[1].queue(self.actual_task[2])
         if self.actual_task[1].customers > self.actual_task[3]:
+            print(
+                self.actual_task[1].name,
+                " skippe with left: ",
+                self.actual_task[1].customers,
+                "time: ",
+                t,
+            )
             self.leave_station(t)
             self.dropped[self.actual_task[1].name] += 1
             self.complete = False
         else:
-            EventQueue.push(Event(t + self.actual_task[1].waiting_time, self.leave_station, t + self.actual_task[1].waiting_time, 1))
+            print(
+                self.actual_task[1].name,
+                "  arrived ",
+                self.actual_task[1].customers,
+                "time: ",
+                t,
+            )
             self.total_time += self.actual_task[1].waiting_time
             self.served[self.actual_task[1].name] += 1
+            EventQueue.push(
+                Event(
+                    t + self.actual_task[1].waiting_time,
+                    self.leave_station,
+                    t + self.actual_task[1].waiting_time,
+                    1,
+                )
+            )
 
     def leave_station(self, t):
         self.actual_task[1].dequeue(self.actual_task[2])
+        print(
+            self.actual_task[1].name,
+            "  departured ",
+            self.actual_task[1].customers,
+            "time: ",
+            t,
+        )
         if self.list:
             self.actual_task = self.list.pop(0)
-            EventQueue.push(Event(t + self.actual_task[0], self.arrive_station, t + self.actual_task[0], 2))
+            EventQueue.push(
+                Event(
+                    t + self.actual_task[0],
+                    self.arrive_station,
+                    t + self.actual_task[0],
+                    2,
+                )
+            )
         else:
             Customer.duration += self.total_time
             if self.complete:
